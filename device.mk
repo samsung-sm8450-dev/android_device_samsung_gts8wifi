691c458ac0714cfaa9bc15c3fdff50fc39a19d90
TARKZiM <tom8476oo@gmail.com>
2026-04-19 17:30:39 +0800

    gts8wifi: audio: Import impl. from sm8550

--- device.mk
+++ device.mk
@@ -25,7 +25,7 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@7.0-impl \
+    android.hardware.audio@7.1-impl.samsung-sm8450 \
     android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl \
